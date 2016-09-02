21228d22f1ca024cd998b9dc741ade42b58115c0
Adam Schoonmaker <adamschoonmaker12@gmail.com>
2016-09-02 15:02:03 -0500

    initial commit; first step of project setup

---
+++ Mooch/Mooch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mooch
+//
+//  Created by adam on 9/2/16.
+//  Copyright © 2016 cse498. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
