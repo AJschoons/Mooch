9f465a133c9e423193460a12a1e4895f95ab30c3
Adam Schoonmaker <adamschoonmaker12@gmail.com>
2016-09-22 21:01:58 -0400

    Updated community to match UML

--- Mooch/Mooch/Community.swift
+++ Mooch/Mooch/Community.swift
@@ -14,29 +14,32 @@ struct Community {
         case Id = "id"
         case Address = "address"
         case Name = "name"
+        case PictureURL = "picture"
     }
     
     let id: Int
     let address: String
     let name: String
+    let pictureURL: String
     
     //Designated initializer
-    init(id: Int, address: String, name: String) {
+    init(id: Int, address: String, name: String, pictureURL: String) {
         self.id = id
         self.address = address
         self.name = name
+        self.pictureURL = pictureURL
     }
     
     //Convenience JSON initializer
     init(json: JSON) throws {
-        guard let id = json[JSONMapping.Id.rawValue].int, let address = json[JSONMapping.Address.rawValue].string, let name = json[JSONMapping.Name.rawValue].string else {
+        guard let id = json[JSONMapping.Id.rawValue].int, let address = json[JSONMapping.Address.rawValue].string, let name = json[JSONMapping.Name.rawValue].string, let pictureURL = json[JSONMapping.PictureURL.rawValue].string else {
             throw InitializationError.insufficientJSONInformationForInitialization
         }
         
-        self.init(id: id, address: address, name: name)
+        self.init(id: id, address: address, name: name, pictureURL: pictureURL)
     }
     
     static func createDummy(fromNumber i: Int) -> Community {
-        return Community(id: i, address: "\(i) Main St.", name: "\(i) Big Apartments")
+        return Community(id: i, address: "\(i) Main St.", name: "\(i) Big Apartments", pictureURL: "http://placehold.it/500x500")
     }
 }
