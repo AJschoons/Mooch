edc3d229bcb6a8ddc510b58154dc7439baedd50d
Adam Schoonmaker <adamschoonmaker12@gmail.com>
2016-09-25 20:14:14 -0500

    added example networking test

--- Mooch/MoochTests/MoochAPITests.swift
+++ Mooch/MoochTests/MoochAPITests.swift
@@ -9,37 +9,59 @@
 import XCTest
 @testable import Mooch
 
+import OHHTTPStubs
+
 class MoochAPITests: XCTestCase {
     
-    func testGETUser_success() {
-        
-        let userId = 2
-        
-        let asynchronousTestExpectation = expectation(description: "the completion closure returns the correct User with no error")
-        var returnedUser: User?
-        var returnedError: Error?
-        
-        MoochAPI.GETUser(withId: userId) { user, error in
-            returnedUser = user
-            returnedError = error
-            asynchronousTestExpectation.fulfill()
-        }
-        
-        waitForExpectations(timeout: 5) { error in
-            //Is a user returned with no error?
-            XCTAssert(returnedError == nil)
-            guard let user = returnedUser else {
-                XCTFail()
-                return
-            }
-            
-            //Is the user's information correct?
-            XCTAssert(user.id == userId)
-        }
+    override func setUp() {
+        super.setUp()
     }
+    
+    override func tearDown() {
+        // Reset all the networking stubs between each test
+        OHHTTPStubs.removeAllStubs()
+        super.tearDown()
+    }
+    
+//    func testGETUser_success() {
+//        
+//        let userId = 2
+//        
+//        let asynchronousTestExpectation = expectation(description: "the completion closure returns the correct User with no error")
+//        var returnedUser: User?
+//        var returnedError: Error?
+//        
+//        MoochAPI.GETUser(withId: userId) { user, error in
+//            returnedUser = user
+//            returnedError = error
+//            asynchronousTestExpectation.fulfill()
+//        }
+//        
+//        waitForExpectations(timeout: 5) { error in
+//            //Is a user returned with no error?
+//            XCTAssert(returnedError == nil)
+//            guard let user = returnedUser else {
+//                XCTFail()
+//                return
+//            }
+//            
+//            //Is the user's information correct?
+//            XCTAssert(user.id == userId)
+//        }
+//    }
     
     func testGETListings_success() {
         
+        let _ = stub(isHost(MoochAPIRouter.baseURLString) && isPath("/listings")) { request in
+            return OHHTTPStubsResponse(
+                fileAtPath: OHPathForFile("json_response/GETListings.json", type(of: self))!,
+                statusCode: 200,
+                headers: [
+                    "ContentType": "application/json"
+                ]
+            )
+        }
+
         let asynchronousTestExpectation = expectation(description: "the completion closure returns the correct Listings with no error")
         var returnedListings: [Listing]?
         var returnedError: Error?
