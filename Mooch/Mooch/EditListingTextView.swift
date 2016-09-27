c810c5feae6164190a45a6f197ceef318a2b1234
Adam Schoonmaker <adamschoonmaker12@gmail.com>
2016-09-27 01:17:43 -0400

    EditListingTextView design is customizable; now looks better

--- Mooch/Mooch/EditListingTextView.swift
+++ Mooch/Mooch/EditListingTextView.swift
@@ -8,7 +8,27 @@
 
 import UIKit
 
+@IBDesignable
 class EditListingTextView: NavigableTextView {
 
     var fieldType: EditListingConfiguration.FieldType!
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
 }
