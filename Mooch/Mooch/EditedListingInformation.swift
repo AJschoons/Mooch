c80fc10687a4a80dc0c2abbcbc8fd61e6243d824
Adam Schoonmaker <adamschoonmaker12@gmail.com>
2016-09-29 00:13:23 -0400

    Added photo handling to Edit Listing screen

--- Mooch/Mooch/EditedListingInformation.swift
+++ Mooch/Mooch/EditedListingInformation.swift
@@ -6,32 +6,32 @@
 //  Copyright © 2016 cse498. All rights reserved.
 //
 
+import UIKit
+
 //Struct to track the edited listing information for the EditListingViewController
 struct EditedListingInformation {
+    
+    var photo: UIImage?
     var title: String?
     var description: String?
     var tag: ListingTag?
     var price: Float?
     var quantity: Int?
     
-    var numberOfInformationFieldsEdited: Int {
-        var num = 0
-        if title != nil { num += 1 }
-        if description != nil { num += 1 }
-        if tag != nil { num += 1 }
-        if price != nil { num += 1 }
-        if quantity != nil { num += 1 }
-        return num
-    }
-    
     var isAllInformationFilled: Bool {
-        return numberOfInformationFieldsEdited == 5
+        if photo == nil || title == nil || tag == nil || price == nil || quantity == nil {
+            return false
+        }
+        
+        return true
     }
     
     func firstUnfilledFieldType() -> EditListingConfiguration.FieldType? {
         var unfilledFieldType: EditListingConfiguration.FieldType? = nil
         
-        if title == nil {
+        if photo == nil {
+            unfilledFieldType = .photo
+        } else if title == nil {
             unfilledFieldType = .title
         } else if description == nil {
             unfilledFieldType = .description
