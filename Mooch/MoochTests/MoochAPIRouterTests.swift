e93db120e1393f93ce447ae608d80c89a924b6a4
Zhiming Jiang <zhimingjiang@Zhimings-MacBook-Pro.local>
2016-10-13 21:57:29 -0400

    API test success

--- Mooch/MoochTests/MoochAPIRouterTests.swift
+++ Mooch/MoochTests/MoochAPIRouterTests.swift
@@ -39,13 +39,10 @@ class MoochAPIRouterTests: XCTestCase {
         
         do {
             let urlRequest = try MoochAPIRouter.getUser(withId: userId).asURLRequest()
-            
             //Does the correct URL get created?
             XCTAssert(urlRequest.url!.absoluteString == "\(MoochAPIRouter.baseURLString)/users/\(userId)")
-            
             //Does the correct URL method get used?
             XCTAssert(urlRequest.httpMethod == GETHttpMethodString)
-            
             //Are the correct parameters getting put into the HTTP body? 
             //Note: these parameters will be JSON encoded in Mooch if there are any
             //(in this case there shouldn't be any)
@@ -58,6 +55,31 @@ class MoochAPIRouterTests: XCTestCase {
             XCTFail()
         }
     }
+    
+    func testGetListingCategoriesRoute() {        
+        do {
+            let urlRequest = try MoochAPIRouter.getListingCategories.asURLRequest()
+            //Does the correct URL get created?
+            XCTAssert(urlRequest.url!.absoluteString == "\(MoochAPIRouter.baseURLString)/categories")
+            //Does the correct URL method get used?
+            XCTAssert(urlRequest.httpMethod == GETHttpMethodString)
+            
+            //Are the correct parameters getting put into the HTTP body?
+            //Note: these parameters will be JSON encoded in Mooch if there are any
+            //(in this case there shouldn't be any)
+            XCTAssert(urlRequest.httpBody == nil)
+            
+            //Are the correct key-value pairs being put into the HTTP headers?
+            //(in this case there are no headers because the route does not require authorization AND there are no JSON parameters sent)
+            XCTAssert(urlRequest.allHTTPHeaderFields!.count == 0)
+        } catch {
+            XCTFail()
+        }
+    }
+    
+    
+    
+    
     
     func testGetListingRoute() {
         
@@ -90,7 +112,6 @@ class MoochAPIRouterTests: XCTestCase {
         
         let email = "email@example.com"
         let password = "not a real password"
-        
         do {
             let urlRequest = try MoochAPIRouter.postLogin(withEmail: email, andPassword: password).asURLRequest()
             
@@ -105,8 +126,8 @@ class MoochAPIRouterTests: XCTestCase {
             XCTAssert(urlRequest.httpBody != nil)
             do {
                 let httpBodyJSON = try convertToJSON(fromHTTPBodyData: urlRequest.httpBody!)
-                XCTAssert(httpBodyJSON[MoochAPIRouter.JSONMapping.PostLogin.email.rawValue].stringValue == email)
-                XCTAssert(httpBodyJSON[MoochAPIRouter.JSONMapping.PostLogin.password.rawValue].stringValue == password)
+                XCTAssert(httpBodyJSON[MoochAPIRouter.ParameterMapping.PostLogin.email.rawValue].stringValue == email)
+                XCTAssert(httpBodyJSON[MoochAPIRouter.ParameterMapping.PostLogin.password.rawValue].stringValue == password)
             } catch {
                 //If there was an error then that means the HTTP body could not be converted to JSON,
                 //so the test should fail because it needs to be valid JSON
@@ -121,4 +142,98 @@ class MoochAPIRouterTests: XCTestCase {
             XCTFail()
         }
     }
+    
+    func testPostListingRoute() {
+        let userId = 1
+        let title = "title"
+        let description = "detail"
+        let price = 2.0
+        let isFree = true
+        let categoryId = 2
+        
+        do {
+            let urlRequest = try MoochAPIRouter.postListing(userId: userId, title: title, description: description, price: Float(price), isFree: isFree, categoryId: categoryId).asURLRequest()
+            
+            //Does the correct URL get created?
+            XCTAssert(urlRequest.url!.absoluteString == "\(MoochAPIRouter.baseURLString)/users/\(userId)/listings")
+            
+            //Does the correct URL method get used?
+            XCTAssert(urlRequest.httpMethod == POSTHttpMethodString)
+            
+            //Are the correct parameters getting put into the HTTP body?
+            //Note: these parameters will be JSON encoded in Mooch if there are any
+            XCTAssert(urlRequest.httpBody != nil)
+            do {
+                let httpBodyJSON = try convertToJSON(fromHTTPBodyData: urlRequest.httpBody!)
+
+                XCTAssert(httpBodyJSON[MoochAPIRouter.ParameterMapping.PostListing.title.rawValue].stringValue == title)
+                XCTAssert(httpBodyJSON[MoochAPIRouter.ParameterMapping.PostListing.description.rawValue].stringValue == description)
+                XCTAssert(httpBodyJSON[MoochAPIRouter.ParameterMapping.PostListing.price.rawValue].doubleValue == price)
+
+                XCTAssert(httpBodyJSON[MoochAPIRouter.ParameterMapping.PostListing.isFree.rawValue].boolValue == isFree)
+                XCTAssert(httpBodyJSON[MoochAPIRouter.ParameterMapping.PostListing.categoryId.rawValue].intValue == categoryId)
+
+            } catch {
+                //If there was an error then that means the HTTP body could not be converted to JSON,
+                //so the test should fail because it needs to be valid JSON
+                XCTFail()
+            }
+            
+            //Are the correct key-value pairs being put into the HTTP headers?
+            //Since we are sending parameters in JSON, there must be a JSON header
+            XCTAssert(urlRequest.allHTTPHeaderFields!.count == 1)
+            XCTAssert(headersContainJSONContentType(headers: urlRequest.allHTTPHeaderFields!))
+        } catch {
+            XCTFail()
+        }
+    }
+    
+
+    func testPostUserRoute() {
+            //Required
+        let communityId = 2
+        let name = "zhiming"
+        let email = "Zhiming@gmail.com"
+        let phone = "ugly face"
+        let password = "444444"
+        let address = "hiden tree"
+        do {
+            let urlRequest = try MoochAPIRouter.postUser(communityId: communityId, name: name, email: email, phone: phone, password: password, address: address).asURLRequest()
+            
+            //Does the correct URL get created?
+            XCTAssert(urlRequest.url!.absoluteString == "\(MoochAPIRouter.baseURLString)/users")
+            
+            //Does the correct URL method get used?
+            XCTAssert(urlRequest.httpMethod == POSTHttpMethodString)
+            
+            //Are the correct parameters getting put into the HTTP body?
+            //Note: these parameters will be JSON encoded in Mooch if there are any
+            XCTAssert(urlRequest.httpBody != nil)
+            do {
+                let httpBodyJSON = try convertToJSON(fromHTTPBodyData: urlRequest.httpBody!)
+                
+                XCTAssert(httpBodyJSON[MoochAPIRouter.ParameterMapping.PostUser.communityId.rawValue].intValue == communityId)
+                XCTAssert(httpBodyJSON[MoochAPIRouter.ParameterMapping.PostUser.name.rawValue].stringValue == name)
+                XCTAssert(httpBodyJSON[MoochAPIRouter.ParameterMapping.PostUser.email.rawValue].stringValue == email)
+                
+                XCTAssert(httpBodyJSON[MoochAPIRouter.ParameterMapping.PostUser.phone.rawValue].stringValue == phone)
+                XCTAssert(httpBodyJSON[MoochAPIRouter.ParameterMapping.PostUser.password.rawValue].stringValue == password)
+                XCTAssert(httpBodyJSON[MoochAPIRouter.ParameterMapping.PostUser.address.rawValue].stringValue == address)
+
+            } catch {
+                //If there was an error then that means the HTTP body could not be converted to JSON,
+                //so the test should fail because it needs to be valid JSON
+                XCTFail()
+            }
+            
+            //Are the correct key-value pairs being put into the HTTP headers?
+            //Since we are sending parameters in JSON, there must be a JSON header
+            XCTAssert(urlRequest.allHTTPHeaderFields!.count == 1)
+            XCTAssert(headersContainJSONContentType(headers: urlRequest.allHTTPHeaderFields!))
+        } catch {
+            XCTFail()
+        }
+    }
+    
+    
 }
